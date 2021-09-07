54a0f8a06b0af1595b45a318aceb9e8ae89028bd
Kateryna Karpova <katyakarpova@gmail.com>
2021-09-07 12:01:27 +0200

    Creating tasks 1 and 2

--- src/App.js
+++ src/App.js
@@ -1,7 +1,45 @@
+import React, { Component } from 'react';
+import { v4 as uuidv4 } from 'uuid';
 import './App.css';
+import ContactsList from './ContactsList';
+import Form from './Form';
 
-function App() {
-  return <div className="App"></div>;
+class App extends Component {
+  state = {
+    contacts: [],
+  };
+
+  // contactId = uuidv4();
+
+  formSubmitHandler = data => {
+    console.log(data);
+
+    const contact = {
+      id: uuidv4(),
+      name: data.name,
+      number: data.number,
+    };
+
+    this.setState(prevState => ({
+      contacts: [contact, ...prevState.contacts],
+    }));
+  };
+
+  render() {
+    const contactsArray = this.state.contacts;
+
+    return (
+      <div className="App">
+        <div>
+          <Form onSubmit={this.formSubmitHandler} />
+        </div>
+        <div>
+          <h2>Contacts</h2>
+          <ContactsList contacts={contactsArray} />
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
